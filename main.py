296cba92fd6be4c38df2a0a7b01a9149633bf629
Badam Neeraj Kumar <69072023+neeraj-badam@users.noreply.github.com>
2025-01-31 22:34:24 +0000

    Added Scraped code

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from scrape import scrape_website
+
+st.title('AI Web Scraper')
+
+url = st.text_input(' Enter a Website URL: ')
+
+if st.button('Scrape site'):
+    st.write('Scrapping the website')
+    result = scrape_website( url )
+
+    print( result )
